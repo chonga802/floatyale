545d9fa75875f431122ef52a221a920abc07a778
Christine Hong <christinehong802@gmail.com>
2014-10-03 19:46:17 -0400

    changed lecture links

--- lectures.php
+++ lectures.php
@@ -11,7 +11,7 @@
 			<p>
 				<a href="https://docs.google.com/a/yale.edu/presentation/d/1I4g4T6Wr3Qmabl3TrQx_jRhIoHNFHG1MLMBZqpza0oI/edit#slide=id.g1274d1570_023"> Click here for HTML/CSS slides</a>
 			</p>
-
+			
 			<h3>Intro to Wordpress</h3>
 			<p>
 				<a href="https://docs.google.com/presentation/d/1b7nPGSAe8LlLaRzab4CmlntxDExAM3GfRT1uqSqYk2g/edit#slide=id.g12664ab9c_0171"> Click here for our Wordpress slides from last year</a>
